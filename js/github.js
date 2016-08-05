dea129e919f471756ba24d93b7a8b4caa7ac878d
Noah Ramey <noahramey1997@gmail.com>
2016-08-05 10:26:00 -0700

    finish main styling

--- js/github.js
+++ js/github.js
@@ -4,9 +4,9 @@ exports.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     for (var i = 0; i < response.length; i++) {
       if (response[i].description === "") {
-        $('#repo-results').append('<li>Repository: ' + response[i].name + ', no description :(</li>');
+        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td>' + response[i].name + '</td><td>no description :(</td></tr>');
       } else {
-        $('#repo-results').append('<li>Repository: ' + response[i].name + ', Description: ' + response[i].description + '</li>');
+        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td>' + response[i].name + '</td><td>' + response[i].description + '</td></tr>');
       }
     }
   }).fail(function(error){
