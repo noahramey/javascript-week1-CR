ab0cf218da92e8fc1a331a0db33d23f2d7640b40
Noah Ramey <noahramey1997@gmail.com>
2016-08-05 11:50:57 -0700

    add links to repositories and fix minor styling issues

--- js/github.js
+++ js/github.js
@@ -3,10 +3,10 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     for (var i = 0; i < response.length; i++) {
-      if (response[i].description === "") {
-        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td>' + response[i].name + '</td><td>no description :(</td></tr>');
+      if (response[i].description === "" || !response[i].description) {
+        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td><a href="' + response[i].html_url + '">' + response[i].name + '</a></td><td>no description :(</td></tr>');
       } else {
-        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td>' + response[i].name + '</td><td>' + response[i].description + '</td></tr>');
+        $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td><a href="' + response[i].html_url + '">' + response[i].name + '</a></td><td>' + response[i].description + '</td></tr>');
       }
     }
   }).fail(function(error){
