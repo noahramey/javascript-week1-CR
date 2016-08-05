bbd2014df883edc6265825225e0edc6b4ad411f3
Noah Ramey <noahramey1997@gmail.com>
2016-08-05 12:20:19 -0700

    add user account detail api call and functionality

--- js/github-form-interface.js
+++ js/github-form-interface.js
@@ -1,11 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 var getRepos = require('./../js/github.js').getRepos;
+var getUser = require('./../js/github.js').getUser;
 
 $(document).ready(function() {
   $('#user-lookup').submit(function(event) {
     event.preventDefault();
     var user = $('#name').val();
     $('#name').val("");
+    $('#user-results').empty();
+    $('#repo-results').empty();
+    getUser(user);
     getRepos(user);
+    $('#users').show();
+    $('#repos').show();
   });
 });
--- js/github.js
+++ js/github.js
@@ -2,6 +2,7 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
+    $('#repo-results').append('<tr class="table-header"><td>Username</td><td>Repository</td><td>Description</td></tr>');
     for (var i = 0; i < response.length; i++) {
       if (response[i].description === "" || !response[i].description) {
         $('#repo-results').append('<tr><td>' + response[i].owner.login + '</td><td><a href="' + response[i].html_url + '">' + response[i].name + '</a></td><td>no description :(</td></tr>');
@@ -10,6 +11,14 @@ exports.getRepos = function(user){
       }
     }
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#repo-results').append('<p>' + error.responseJSON.message + '</p>');
+  });
+};
+
+exports.getUser = function(user){
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+    $('#user-results').append('<p>Username: ' + response.login + '</p><p>Name: ' + response.name + '</p><p>Location: ' + response.location + '</p>');
+  }).fail(function(error){
+    $('#user-results').append('<p>' + error.responseJSON.message + '</p>');
   });
 };
